8b7fb1b58514db26a8c34bac6dd7d31594c41807
galamo <galamouyal88@gmail.com>
2024-08-28 20:03:39 +0300

    a

--- BankZero/api/src/index.ts
+++ BankZero/api/src/index.ts
@@ -1,12 +1,56 @@
 import express, { Response } from "express"
 import dotenv from "dotenv"
+import cors from "cors"
+import bodyParser from "body-parser"
 dotenv.config()
 
 const app = express()
 
+app.use(bodyParser.json())
+app.use(cors())
+
+const accounts: { [accountId: number]: number } = {};
+const history = []
+
 app.get("/", (req, res, next) => {
     res.send("Api is Ok")
 })
+
+enum Operations {
+    DEPOSIT = "deposit",
+    WITHDRAW = "withdraw"
+}
+
+app.post("/account/operation", (req, res, next) => {
+    const data = req.body
+    const { accountId, amount, operation } = data as { accountId: number, amount: number, operation: Operations };
+    history.push({ accountId, operation, amount, timeStamp: new Date().toISOString() })
+    if (operation?.toLowerCase() === Operations.DEPOSIT) {
+        if (accounts[accountId]) {
+            accounts[accountId] = amount + accounts[accountId];
+        } else {
+            history.push({ accountId, operation: "newAccount", amount, timeStamp: new Date().toISOString() })
+            accounts[accountId] = amount;
+        }
+        res.json({ accountId, amount: accounts[accountId] })
+    } else if (operation?.toLowerCase() === Operations.WITHDRAW) {
+        if (accounts[accountId]) {
+            accounts[accountId] = accounts[accountId] - amount;
+        } else {
+            history.push({ accountId, operation: "newAccount", amount, timeStamp: new Date().toISOString() })
+            accounts[accountId] = -amount;
+        }
+        res.json({ accountId, amount: accounts[accountId] })
+    } else {
+        return res.status(403).json({ message: "operation is not permitted" })
+    }
+
+
+})
+
+
+
+
 
 
 app.listen(process.env.API_PORT, () => {
--- BankZero/bank-client/src/App.tsx
+++ BankZero/bank-client/src/App.tsx
@@ -1,11 +1,32 @@
+import { useEffect } from "react"
 import './App.css'
 
+import axios from 'axios'
 function App() {
+
+    async function submitTransaction() {
+        try {
+            const result = await axios.post("http://localhost:3500/account/operation", {
+                accountId: "12345",
+                amount: 600,
+                operation: "withdraw"
+            })
+            console.log(result)
+        } catch (error) {
+            alert("error")
+        }
+    }
+
+    useEffect(() => {
+        submitTransaction()
+        return () => {
+        }
+    }, [])
 
     return (
         <>
             <div>
-            
+                <h1> Client </h1>
             </div>
         </>
     )
